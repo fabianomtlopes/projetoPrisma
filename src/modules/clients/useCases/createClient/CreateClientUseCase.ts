28aab15ec35fc42d9bdebcb6b69885255c4c3b2e
Fabiano de Matos Lopes <fabianomtlopes@yahoo.com.br>
2022-06-07 20:42:05 -0300

    Projeto Prisma - Client

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,19 @@
+
+
+interface ICreateClient {
+  username: string;
+  password: string;
+}
+
+
+export class CreateClientUseCase {
+
+  async execute({username, password}: ICreateClient){
+      //Validar se o usuario existe
+
+      //Criptografar a senha
+
+      //Salvar o cliente
+  }
+
+}
